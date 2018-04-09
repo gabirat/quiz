28782644f3a335928c00a9ed2322bc321b8f5864
minttuNB <cerealley@gmail.com>
2018-04-09 10:05:27 +0200

    Backbone server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+/* eslint-disable no-undef */
+'use strict'
+const config = require("./config");
+const express = require("express");
+const app = express();
+const path = require("path");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = config.port;
+/* eslint-disable no-console */
+server.listen(port, ()=>{
+	console.log(`[server] Listening on port ${port}...`);
+});
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/js", express.static(__dirname+"/node_modules/socket.io-client/dist"));
