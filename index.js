5ce65c02f5ba5b0d5ba32ce8db34629d34d805ec
minttuNB <cerealley@gmail.com>
2018-04-09 10:33:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-'use strict'
 const config = require("./config");
 const express = require("express");
 const app = express();
@@ -13,3 +12,29 @@ server.listen(port, ()=>{
 });
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/js", express.static(__dirname+"/node_modules/socket.io-client/dist"));
+let users = [];
+io.on("connection", socket =>{
+	let registered = false;
+	socket.on("user-join", username =>{
+		if(registered) return;
+		if(username.length>25){
+			username = username.slice(0,25);
+		}
+		socket.username = username;
+		users[socket.username+"."+socket.id] = {
+			joined: new Date(), 
+			username: username,
+			id: socket.id,
+			ip: socket.request.connection.remoteAddress,
+			correctAnswers: 0
+		};
+		console.log(`[server] New user: ${users[username+"."+socket.id]}`);
+		registered = true;
+	});
+	socket.on("user-leave", () => {
+		if(registered){
+			delete users[socket.username+"."+socket.id];
+			console.log(`[server] User disconnected: ${socket.username}`);
+		} 
+	});
+});
