36c7a470b8811ffa8e58f5c5173940ca26747ff6
gabirat <gabirat@onet.pl>
2018-04-16 00:14:31 +0200

    colors work again after question change

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,33 @@
 /* eslint-disable no-undef */
 let submit;
+
 $(document).ready(function(){
 	let socket,
 		username,
 		answered = false;
+
 	function stripHTMLTags (input) {
 		return $("<div/>").text(input).html();
 	}
+
+	const QuestionDOM = { //question structure
+		answers: [$("#ans0"),$("#ans1"),$("#ans2"),$("#ans3")],
+		content: $($(".questionContent")[0]),
+		questionNo: $($(".questionNo")[0])
+	};
+	
+	function colorfulButtons() {
+		console.log("Chnged colors");
+		QuestionDOM.answers[0].css({"background": "red", "color": "white"});
+		QuestionDOM.answers[1].css({"background": "rgb(173, 156, 0)", "color": "white"});
+		QuestionDOM.answers[2].css({"background": "rgb(0, 49, 139)", "color": "white"});
+		QuestionDOM.answers[3].css({"background": "rgb(120, 0, 136)", "color": "white"});
+	}
+
 	function register () {
 		let name = stripHTMLTags($("#name").val().trim());
 		let surname = stripHTMLTags($("#surname").val().trim());
 		let school = $("#school").val().trim();
-
-		const QuestionDOM = { //question structure
-			answers: [$("#ans0"),$("#ans1"),$("#ans2"),$("#ans3")],
-			content: $($(".questionContent")[0]),
-			questionNo: $($(".questionNo")[0])
-		};
 
 		username = `${name} ${surname}`;
 		if (username) {
@@ -25,10 +36,12 @@ $(document).ready(function(){
 				username: username,
 				school: school
 			});
+
 			socket.on("not-ready", ()=>{
 				$($(".title_page")[0]).hide();
 				$($(".not-ready")[0]).show();
 			});
+
 			socket.on("ready", ()=>{
 				$($(".not-ready")[0]).hide();
 				$($(".quiz")[0]).show();
@@ -43,6 +56,7 @@ $(document).ready(function(){
 					}
 				}
 				socket.on("next-question", data =>{
+					colorfulButtons();
 					answered = false;
 					QuestionDOM.content.text(data.content);
 					for(let i in QuestionDOM.answers){
